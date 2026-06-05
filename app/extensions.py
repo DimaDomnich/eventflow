6c66433d7b4b06d7fc47b26e470f18e11e7d5fbd
Dmytro Domnich <d.domnich2017@gmail.com>
2026-06-05 12:18:17 +0300

    add SES service to send emails on waitlist notification

--- app/extensions.py
+++ app/extensions.py
@@ -31,3 +31,12 @@ def get_s3_client():
         aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
         region_name=os.getenv("AWS_REGION"),
     )
+
+
+def get_ses_client():
+    return boto3.client(
+        "ses",
+        aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+        aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+        region_name=os.getenv("AWS_REGION"),
+    )
--- app/tasks/waitlist.py
+++ app/tasks/waitlist.py
@@ -1,6 +1,9 @@
+import os
+
 from app.celery_app import celery
+from app.models.user import UserModel
 from app.models.waitlist import WaitlistModel
-from app.extensions import db
+from app.extensions import db, get_ses_client
 from datetime import datetime, timedelta, timezone
 
 
@@ -18,13 +21,28 @@ def notify_next_waitlist_person(event_id):
     if not waitlist:
         return
 
+    ses = get_ses_client()
+
+    user = UserModel.query.get(waitlist.user_id)
+
+    ses.send_email(
+        Source=os.getenv("SES_SENDER"),
+        Destination={"ToAddresses": [user.email]},
+        Message={
+            "Subject": {"Data": "A spot opened up for your waitlisted event"},
+            "Body": {
+                "Text": {
+                    "Data": f"Hi {user.full_name}, a ticket is now available. You have 5 minutes to purchase before the next person is notified."
+                }
+            },
+        },
+    )
+
     waitlist.notified_at = datetime.now(timezone.utc)
 
     waitlist.expired_at = waitlist.notified_at + timedelta(minutes=5)
 
     db.session.commit()
-
-    print(f"Notifying user {waitlist.user_id} for event {event_id}")
 
 
 @celery.task
